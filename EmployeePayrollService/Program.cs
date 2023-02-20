7edca3a52afd02d0402c8c54cdc21b3311ef7c40
Shailendra kumar pal <palshailendrakumar123@gmail.com>
2023-02-20 22:10:32 +0530

    first commit

---
+++ EmployeePayrollService/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EmployeePayrollService
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
